2390d1b1316d6b60a789095ed09cef1f31898182
Fernando Contreras <fercreek@gmail.com>
2015-10-12 03:34:09 -0500

    Nested serializer example

--- examples/views.py
+++ examples/views.py
@@ -4,14 +4,12 @@ from rest_framework.response import Response
 from .models import Ingredient, Recipe
 from .serializers import IngredientSerializer, RecipeSerializer
 
+# Based in this question
+# http://stackoverflow.com/questions/28078092/django-rest-framework-writable-nested-serializers
+
 class IngredientView(viewsets.ModelViewSet):
-    """
-    This viewset automatically provides `list`, `create`, `retrieve`,
-    `update` and `destroy` actions.
-    """
     queryset = Ingredient.objects.all()
     serializer_class = IngredientSerializer
-    # permission_classes = (permissions.IsAuthenticatedOrReadOnly, IsOwnerOrReadOnly,)
 
 class RecipeView(viewsets.ModelViewSet):
     queryset = Recipe.objects.all()
